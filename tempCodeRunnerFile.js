d89a0a47b581384686ddf7cbfd0f900f5897c710
Rugroso <133253941+Rugroso@users.noreply.github.com>
2024-03-07 14:40:05 -0800

    Logica implementada para el lado del cliente

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,25 +1,42 @@
-const fs = require('fs');
-fs.readFile('personajes.json', 'utf8', (error, data) => {
-    if (error) {
-      console.error("Error al leer el archivo:", error);
-      return;
-    }
-    
-    const personasICC = JSON.parse(data);
-    
-    const origens = "San Luis";
-    
-    /*personas.forEach(persona => {
+function logica () {
+    let Pregunta1 = document.getElementById("Pregunta1").value;
+    let contenido = "";
+    //let Pregunta2 = document.getElementById("Pregunta2").value;
+    //let Pregunta3 = document.getElementById("Pregunta3").value;
+    //let Pregunta4 = document.getElementById("Pregunta4").value;
+    //let Pregunta5 = document.getElementById("Pregunta5").value;
+    //let Pregunta6 = document.getElementById("Pregunta6").value;
+    //let Pregunta7 = document.getElementById("Pregunta7").value;
+    //let Pregunta8 = document.getElementById("Pregunta8").value;
+    //let Pregunta9 = document.getElementById("Pregunta9").value;
+    //let Pregunta10 = document.getElementById("Pregunta10").value;  
+        
+        /*personas.forEach(persona => {
         console.log(`${persona.Nombre} ${persona.Origen}`);
-    });
-    */
-   
-    personasICC.forEach(tilin => {
-        if (tilin.Origen == "Tijuana") {
-            tilin.Acumulado++;
-        }
-        if (tilin.Acumulado == 1) {
-            console.log(tilin);
-        }
-    });    
-  });
+        });
+        */
+    const API_URL = "personajes.json";
+    async function leerJson() {
+    try {
+        const response = await fetch(API_URL);
+        const personasICC = await response.json();
+        console.log(personasICC); // Agrega esto en tu función leerJson para depurar
+
+        personasICC.forEach(tilin => {
+            if (tilin.Origen == Pregunta1) {
+                tilin.Acumulado++;
+            }
+        });
+        personasICC.forEach(tilin => {
+            if (tilin.Acumulado == 1) {
+                contenido += tilin.Nombre;
+            }
+        });
+        document.getElementById('texto').innerHTML = contenido;
+    } catch (error) {
+        console.error('Error al cargar o procesar el JSON:', error);
+    }
+    }
+    leerJson();
+    console.log(contenido);
+    };
