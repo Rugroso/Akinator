61ba1189e6ffc2065f590237e267f42f8d1af942
Rugroso <133253941+Rugroso@users.noreply.github.com>
2024-03-12 19:54:54 -0700

    Logica casi completada y front casi listo

--- Script.js
+++ Script.js
@@ -1,69 +1,121 @@
-let arreglo = [-1];
-for (let i = 1; i <= 10; i++) {
-    document.getElementById(`Pregunta${i}`).style.display = 'none';
+let arreglo = [];
+let sino = "";
+let talvez=0;
+let cont=0;
+let tilines = [];
+
+async function leerJson() {
+    try {
+        const API_URL = "personajes.json";
+        const response = await fetch(API_URL);
+        tilines = await response.json();
+        console.log("Tilines cargados:", tilines);
+    } catch (error) {
+        console.error('Error al cargar los datos de los personajes:', error);
+    }
 }
+
+function buttonSi() {
+    sino="Si";
+    logica();
+}
+
+function buttonNo() {
+    sino=false;
+    sino="No";
+    logica();
+}
+
+function buttonTalvez() {
+    if (talvez%2===0) {
+        sino=="Si";
+    }
+    else {
+        sino=="No";
+    }
+    talvez++;
+    logica();
+}
+
+
+function cargar() {
+    for (let i = 1; i <= 2; i++) {
+        document.getElementById(`Pregunta${i}`).style.display = 'none';
+    }
+    document.getElementById("Si").style.display = 'none';
+    document.getElementById("No").style.display = 'none';
+    document.getElementById("Talvez").style.display = 'none';
+    document.getElementById("Texto").style.display = 'none';
+    document.getElementById("preguntaTitulo").style.display = 'none';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    leerJson();
+});
+
+function iniciar() {
+    let numRandom = 1 + parseInt(Math.random()*2);
+    arreglo[0]=numRandom;
+    document.getElementById(`Pregunta${numRandom}`).style.display = 'block';
+    document.getElementById("Si").style.display = 'block';
+    document.getElementById("No").style.display = 'block';
+    document.getElementById("Talvez").style.display = 'block';
+    document.getElementById(`Bienvenida`).style.display = 'none';
+}
+
 function logica () {
-    if (arreglo.length<11) {
+    let contenido = "";
+    if (arreglo.length<3) {
+        for (let i = 1; i <= 2; i++) {
+            document.getElementById(`Pregunta${i}`).style.display = 'none';
+        }
         let numRandom = 0;
         let vf = true;
         let encontrar = false;
-        while (vf===true) {
-            encontrar=false;
-            numRandom = 1 + parseInt(Math.random()*10);
-            console.log(`Se genero este numero: ${numRandom}`);
-            for (let recorrer of arreglo) {
-                if (recorrer===numRandom) {
-                    encontrar = true;
+        if (arreglo.length<2) {
+            while (vf===true) {
+                encontrar=false;
+                numRandom = 1 + parseInt(Math.random()*2);
+                console.log(`Se genero este numero: ${numRandom}`);
+                for (let recorrer of arreglo) {
+                    if (recorrer===numRandom) {
+                        encontrar = true;
+                    }
+                } 
+                if (encontrar===false) {
+                    arreglo.push(numRandom);
+                    console.log(`Tamaño del arreglo: ${arreglo.length-1}`);
+                    break;
                 }
-            } 
-            if (encontrar===false) {
-                arreglo.push(numRandom);
-                console.log(`Tamaño del arreglo: ${arreglo.length-1}`);
-                break;
             }
+            let preguntaRandom = `Pregunta${numRandom}`;
+            document.getElementById(preguntaRandom).style.display = 'block';
         }
 
-        let preguntaRandom = `Pregunta${numRandom}`;
-        document.getElementById(preguntaRandom).style.display = 'block';
-        let contenido = "";
-        
-        let Pregunta1 = document.getElementById("Pregunta1").value;
-        let Pregunta2 = document.getElementById("Pregunta2").value;
-        let Pregunta3 = document.getElementById("Pregunta3").value;
-        let Pregunta4 = document.getElementById("Pregunta4").value;
-        let Pregunta5 = document.getElementById("Pregunta5").value;
-        let Pregunta6 = document.getElementById("Pregunta6").value;
-        let Pregunta7 = document.getElementById("Pregunta7").value;
-        let Pregunta8 = document.getElementById("Pregunta8").value;
-        let Pregunta9 = document.getElementById("Pregunta9").value;
-        let Pregunta10 = document.getElementById("Pregunta10").value;  
-            
-        /*personas.forEach(persona => {
-        console.log(`${persona.Nombre} ${persona.Origen}`);
-        });
-        */
-
-        const API_URL = "personajes.json";
-        const leerJson = async() => {
-        try {
-            const response = await fetch(API_URL);
-            const personasICC = await response.json();
-
-            personasICC.forEach(tilin => {
-                if (tilin.Sigma == Pregunta1) {
-                    tilin.Acumulado++;
-                }
+        const analizarPersonajes = (personajesICC) => {
+            personajesICC.forEach(tilin => {
+                Object.keys(tilin).forEach((propiedad, pos) => {
+                    if (tilin[propiedad] === sino && pos === arreglo[cont]) {
+                        tilin.Acumulado++;
+                        console.log(tilin.Acumulado);
+                    }
+                });
             });
-            personasICC.forEach(tilin => {
-                if (tilin.Acumulado == 1) {
+
+            personajesICC.forEach(tilin => {
+                if (tilin.Acumulado == 2) {
+                    console.log(tilin.Nombre)
                     contenido += `${tilin.Nombre}<br>`;
-                }
+                    document.getElementById(`Texto`).style.display = 'block';
+                    document.getElementById("Si").style.display = 'none';
+                    document.getElementById("No").style.display = 'none';
+                    document.getElementById("Talvez").style.display = 'none';
+              }
             });
-            document.getElementById('texto').innerHTML = contenido;
-        } catch (error) {
-            console.error('Error al cargar o procesar el JSON:', error);
+
+            document.getElementById('Texto').innerHTML = contenido;
+            cont++;
         }
-        }
-        leerJson();
+        analizarPersonajes(tilines);
     }
     };
